97009d6937afabf3807c410d058dd8d75fb5b026
cadu.coelho@gmail.com <cadu.coelho@gmail.com@60abb7cb-7982-40a5-7776-63477c5735a5>
2012-02-21 17:48:27 +0000

    Initial commit.

---
+++ trunk/feeds/f_folhaonline.py
@@ -0,0 +1,19 @@
+import feedparser
+import re
+from basefeed import BaseFeed
+
+class Feed(BaseFeed):
+	def __process__(self):
+		for entry in self.feedrawdata['entries']:
+			item = []
+			item.append(entry.title)
+			
+			textdata = re.sub(r'\n','',re.sub(r'\.\.\..?$','',re.sub(r'</?.+>','', entry.description)))
+                        textdata = re.sub(r'\(.+\)\W?','',textdata) ### Remove stuff inside parenthesis
+                        textdata = textdata.replace('\n','') ### Remove newlines inside the code
+			textdata = textdata.replace('\t','')
+
+			item.append(textdata.strip())
+			
+			self.entries.append(item)
+
