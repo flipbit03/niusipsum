efa29f37022469b4a05ee45ddf98ba642f99065d
cadu.coelho@gmail.com <cadu.coelho@gmail.com@60abb7cb-7982-40a5-7776-63477c5735a5>
2012-02-25 04:07:46 +0000

    Added randomization functions, work started on poem generation logic.

--- trunk/niusipsum.py
+++ trunk/niusipsum.py
@@ -7,7 +7,9 @@ import sqlite3
 data = open('sampledata/mixer.data.txt','rb').readlines()
 
 class NiusIpsum:
-    def __init__(self, data, datacodec='utf-8', sectcount=4, sectlinecount=4, wordsperline=5):
+# --------------- constructor
+
+    def __init__(self, data, datacodec='utf-8', sectcount=4, sectlinecount=4, wordsperline=5, diversity=4):
         # store outside params
         self.data = []
         for line in data:
@@ -15,24 +17,43 @@ class NiusIpsum:
         self.sectcount = sectcount
         self.sectlinecount = sectlinecount
         self.wordsperline = wordsperline
+        self.diversity = max(2, diversity) # affects how many rhyme components are used
 
         # we can now work
-        self.isinit =  True
+        self.isinit = True
 
         # storage for sentences
         self.pool = []
         self.rhymepool = {}
 
+# --------------- randomization support
+
+    def roll(self, sides, minimum=1):
+		return max(minimum, int(random.random()*sides + 1) )
+
+    def popnr(self, sourcelist, howmany=1):
+		out = []
+		src = sourcelist
+		if howmany > len(sourcelist):
+			raise Exception("NiusIpsum popnr(): HowMany > len(sourcelist) ???")
+		else:
+			for i in range(howmany):
+				popped = src.pop(self.roll(len(src),minimum=0)-1)
+				out.append(popped)
+		return out
+
+# --------------- text munging functions
+
     def process(self):
         if not self.isinit:
-            raise Exception("NiusIpsum isinit = False!")
+            raise Exception("NiusIpsum process(): isinit = False!")
         else:
             print "<NiusIpsum() Processing>"
             for line in self.data:
                 print "#",
                 sectionizedstring = self.sectionize(line, self.wordsperline)
                 for ss in sectionizedstring:
-                    rhyme = ss[-2:]
+                    rhyme = ss[-2:].lower()
                     self.pool.append( ( ss, self.sscore(ss), rhyme ) )
                     if not self.rhymepool.has_key(rhyme):
                         self.rhymepool[rhyme] = []
@@ -42,7 +63,7 @@ class NiusIpsum:
                     print ".",
 
     def sectionize(self, t, wc=4):
-        regexp = r'(?=(\b%s\w\w\w\w+))' % (r'\w+\s+'*(wc-1))
+        regexp = r'(?=(\b%s\w\w\w+))' % (r'\w+\s+'*(wc-1)) #notice the last word is at LEAST 3 chars \w\w\w+
         return re.findall(regexp, t, re.UNICODE)
 
     def sscore(self, t, wlen=3):
@@ -64,5 +85,55 @@ class NiusIpsum:
 
         return score
 
+# --------------- poem generation process
+    
+    def poem(self,a=10):
+		totalscores = [ (x,y,g) for x in self.rhymepool.keys() for y in [sum([z[1] for z in self.rhymepool[x]])] for g in [len(self.rhymepool[x])] ]
+		totalscores_sorted = sorted(totalscores, key = lambda sumscore: sumscore[1])[::-1]	# Sort by x[1] which is SCORE, and invert for DESCENDING with [::-1]
+		return totalscores_sorted
+
+	
+    def generatepoemscript(self, sortedscorelist):
+		# script
+		thescript = []
+
+		# how many rhymes per section
+		rps = self.sectlinecount // 4
+
+		# how many single sentences per section
+		sss = self.sectlinecount % 4
+		
+		# how many rhymes (read, 4 intertwined sentences) do we need?
+		totalrc = (rps) * self.sectcount
+
+		#adjust diversity
+		adjdivers = min(self.roll(self.diversity, minimum=2), len(sortedscorelist))
+
+		#get elements using adjusted diversity as a cutoff
+		try:
+			elements = [x[0] for x in sortedscorelist[0:adjdivers]]
+		except:
+			raise Exception("NiusIpsum generatepoemscript(): adjdivers > len(scorelist)")
+
+		for section in range(self.sectcount):
+			#SECTION
+			pass
+
+		return elements
+
+
+
+
+
+
+
+
+
 b = NiusIpsum(data)
 b.process()
+
+z = b.poem()
+
+exi = b.generatepoemscript(z)
+
+print 'exi --> ' + repr(exi)
